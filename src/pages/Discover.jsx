bc7288ced823e6cd4e3c2adc96f0460b06522b90
David Moina <davemolina777@gmail.com>
2023-07-01 22:09:47 -0500

    Create basic title and div styling

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,16 @@
-import { Error, Loader, SongCard } from '../components'
-import { genres } from '../assets/constants'
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
 const Discover = () => {
+    const genreTitle = 'Pop';
     return (
         <div className='flex flex-col'>
-            Discover
+            <div className='w-full flex justify-between items-center sm: flex-row flex-col mt-4 mb-10'>
+            <h2 className='font-bold text-3xl text-white text-left'>
+                Discover {genreTitle}
+                </h2>
+            <select> </select>
+            </div>
         </div>
     )
 }

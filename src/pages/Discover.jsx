d7288a8a28449564899e5493a3f40b22f39e1265
David Moina <davemolina777@gmail.com>
2023-07-01 22:18:35 -0500

    Create temporary song iterator

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -16,8 +16,13 @@ const Discover = () => {
                 onChange={handleOnChange()}
                 value=''
                 className='bg-black text-gray-300 rounded-lg outline-none p-3 text-sm sm:mt-0 mt-5'> 
-                {genres.map((genre) => <option> genre.title</option>)}
+                {genres.map((genre) => <option key={genre.title} value={genre.value}> {genre.title}</option>)}
             </select>
+            </div>
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {[1,2,3,4,5,6,7,8,9,10].map((song) => 
+                    <SongCard key={song.key} song={song} i={i}/>
+                )}
             </div>
         </div>
     )

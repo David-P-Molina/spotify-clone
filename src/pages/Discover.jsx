7f450b93551d05766784aa8b8e27a18ca9f40f0f
David Moina <davemolina777@gmail.com>
2023-07-01 22:13:32 -0500

    Create onChange function and map over options

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,13 +3,21 @@ import { genres } from '../assets/constants';
 
 const Discover = () => {
     const genreTitle = 'Pop';
+    const handleOnChange = () => {
+
+    }
     return (
         <div className='flex flex-col'>
             <div className='w-full flex justify-between items-center sm: flex-row flex-col mt-4 mb-10'>
             <h2 className='font-bold text-3xl text-white text-left'>
                 Discover {genreTitle}
                 </h2>
-            <select> </select>
+            <select 
+                onChange={handleOnChange()}
+                value=''
+                className='bg-black text-gray-300 rounded-lg outline-none p-3 text-sm sm:mt-0 mt-5'> 
+                {genres.map((genre) => <option> genre.title</option>)}
+            </select>
             </div>
         </div>
     )
